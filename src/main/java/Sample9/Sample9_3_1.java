a62a69749654845495129f6fcbfe1ebfee5d872b
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-20 11:26:49 +0900

    プログラム例　ゼロ除算による例外が発生するプログラムSample9_3_1.javaを作成しました

---
+++ src/main/java/Sample9/Sample9_3_1.java
@@ -0,0 +1,8 @@
+public class Sample9_3_1 {
+    public static void main(String[] args) {
+        int i,j;
+        i = 0;                                  //①
+        j = 10 / i;                             //②
+        System.out.println("整数値:" + i);      //③
+    }
+}
