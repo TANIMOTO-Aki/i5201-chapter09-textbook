215d5020953699143ad193853f1bf162c95b4dec
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-20 11:34:21 +0900

    プログラム例　文字列の数値変換の際、形式が不正であるため例外が発生するプログラムSample9_3_2.javaを作成しました

---
+++ src/main/java/Sample9/Sample9_3_2.java
@@ -0,0 +1,8 @@
+public class Sample9_3_2 {
+    public static void main(String[] args) {
+        int i;
+        String st = "1.5";                  //①
+        i = Integer.parseInt(st);           //②
+        System.out.println("整数値:" + i);  //③
+    }
+}
