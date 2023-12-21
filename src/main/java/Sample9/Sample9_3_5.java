2d726101b99190b0b63604657dd155f2d61babe6
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-21 10:29:59 +0900

    プログラム例　複数種類の例外をマルチキャッチするプログラムSample9_3_5.javaを作成しました

---
+++ src/main/java/Sample9/Sample9_3_5.java
@@ -0,0 +1,16 @@
+public class Sample9_3_5 {
+    public static void main(String[] args) {
+        int i = 0, j = 0;
+        String st = null;
+        try {
+            st = args[0];
+            i = Integer.parseInt(st);
+            j = 10 / i;
+        } catch (NumberFormatException |
+                 ArrayIndexOutOfBoundsException |
+                 ArithmeticException e) {
+            System.out.println("例外が発生しました:" + e);
+        }
+        System.out.print("整数値:" + i);
+    }
+}
